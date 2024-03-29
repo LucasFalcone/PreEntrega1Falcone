6cf8f1bcee8f8e51f3db48fb874b4ab062e70c89
LucasFalcone <lucasfalcone841@gmail.com>
2024-03-29 19:04:58 -0300

    Primer entrega

---
+++ suplemengod/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import UserCard from '../components/ProductCard'
+
+function Main() {
+    return (
+        <main className='p-4 grow'>
+            <h2>Productos</h2>
+            
+            <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 users">
+                <UserCard name="Proteina ENA" />
+                <UserCard name="Proteina Star" />
+                <UserCard name="Proteina BSN" />
+            </section>
+        </main>
+    )
+}
+
+export default Main
