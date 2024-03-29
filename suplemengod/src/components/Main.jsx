ba9bd9a7c87401e22b476cbf887c6abbe850145e
LucasFalcone <lucasfalcone841@gmail.com>
2024-03-29 19:23:18 -0300

    primer pre entrega

--- suplemengod/src/components/Main.jsx
+++ suplemengod/src/components/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
             
             <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 users">
                 <UserCard name="Proteina ENA" />
-                <UserCard name="Proteina Star" />
+                <UserCard name="Proteina STAR" />
                 <UserCard name="Proteina BSN" />
             </section>
         </main>
